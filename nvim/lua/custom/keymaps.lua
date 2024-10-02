5d06ba67a4a5f923baab174a6760b59bd5beb3df
h <35790175+hiunshim@users.noreply.github.com>
2024-10-02 00:05:33 -0700

    comment out unused commands

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>ls', vim.cmd.Ex)
 
 -- move highlighted texts up and down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
@@ -31,16 +31,17 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
 
 -- yank the whole file
-vim.keymap.set({ 'n', 'v' }, '<leader>ya', '<cmd>%y+<CR>')
+-- vim.keymap.set({ 'n', 'v' }, '<leader>ya', '<cmd>%y+<CR>')
 -- when pasting over a text, keeps previously copied text in buffer
--- vim.keymap.set("x", "<leader>p", [["_dP]])
--- copies to system clipboard and separates it from yanked text in vim
--- vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
--- vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set('x', '<leader>p', [["_dP]])
+-- yank to clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
 -- delete without putting into register
--- vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+vim.keymap.set({ 'n', 'v' }, '<leader>dd', [["_dd]])
 -- start replacing the word under cursor
--- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- make current file executable
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
